70be6a4472a53471162091537aa9cad525ed76d4
Dattatray8 <dattatraymahindrakar@gmail.com>
2025-02-15 22:23:17 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Categories from './components/Categories';
 
 function App() {
   return (
     <div>
        <Navbar />
+       <Categories />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,30 @@
 import './Navbar.css';
 import { TiShoppingCart } from "react-icons/ti";
 import { BsSearch } from "react-icons/bs";
+import { VscColorMode } from "react-icons/vsc";
+import { FiShoppingBag } from "react-icons/fi";
 
-function Navbar(){
+function Navbar() {
     return (
         <div className="nav">
             <div className='nav-icon'>
-            <TiShoppingCart />
+                <TiShoppingCart />
             </div>
             <b>Shopio</b>
+            <form className='search'>
             <input type='text' placeholder='Search Shopio..' id='search-box' />
-            <div className="search-icon">
-            <BsSearch />
-            </div>
+            <BsSearch id='search-icon' />
+            </form>
+            
             <div className='login'>
-            <a href="/login">Login</a><span></span>
-            <a href="/SignUp">SignUp</a>
+                <a href="/login">Login</a><span></span>
+                <a href="/SignUp">SignUp</a>
+            </div>
+            <div className='theme'>
+                <VscColorMode />
+            </div>
+            <div className='cart-icon'>
+                <FiShoppingBag />
             </div>
         </div>
     );
